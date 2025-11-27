675835714aec6f0798bf60150aaa47dae04b1de8
Dhiraj Chaudhary <dhiraj@lolofit.com>
2025-11-27 16:31:50 +0530

    fix: prevent nil.split crash on custom delimiter edge cases

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -20,12 +20,17 @@ class StringCalculator
     numbers = input
 
     if input.start_with?("//")
-      parts = input.split("\n", 2)
-      delimiter = Regexp.escape(parts[0][2..])
-      delimiter = Regexp.new(delimiter)
-      numbers = parts[1]
+      if input.include?("\n")
+        delimiter_line, numbers = input.split("\n", 2)
+        delim_char = delimiter_line[2..-1]
+        delimiter = Regexp.escape(delim_char)
+        delimiter = Regexp.new(delimiter)
+      else
+
+        numbers = input
+      end
     end
 
-    [delimiter, numbers]
+    [delimiter, numbers || ""]
   end
 end
