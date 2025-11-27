eea1dd34fbe06ed99be9971709a56328023718bb
Dhiraj Chaudhary <dhiraj@lolofit.com>
2025-11-27 15:48:20 +0530

    feat: raise exception with all negative numbers (GREEN)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,24 @@
 class StringCalculator
   def self.add(numbers)
-    return 0 if numbers.empty?
-    
+    return 0 if input.empty?
+
     delimiter = /,|\n/
-    numbers.split(delimiter).sum(&:to_i)
+    numbers = input
+
+    if input.start_with?("//")
+      delimiter_line, numbers = input.split("\n", 2)
+      delimiter_char = delimiter_line[2..]
+      delimiter = Regexp.escape(delimiter_char)
+      delimiter = Regexp.new(delimiter)
+    end
+
+    nums = numbers.split(delimiter).map(&:to_i)
+    negatives = nums.select { |n| n < 0 }
+
+    unless negatives.empty?
+      raise "negative numbers not allowed #{negatives.join(',')}"
+    end
+
+    nums.sum
   end
 end
